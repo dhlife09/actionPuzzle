16c170b8ee1a6d9745ff280415d3298d4e5e2bda
DHLife <dhlife09@gmail.com>
2016-10-20 18:49:40 +0900

    Add files via upload

---
+++ BlockBlue.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockBlue : MonoBehaviour {
+	public float rotateSpeed=60f;
+	private bool isRotate=false;
+	void OnMouseDown () {
+		if (isRotate)
+			return;
+		isRotate = true;
+		StartCoroutine ("RotateDestory");
+	}
+
+
+	IEnumerator RotateDestory(){
+		float angle = 0;
+		while (angle<90) {
+			angle=angle+rotateSpeed*Time.deltaTime;
+			transform.Rotate (0, rotateSpeed * Time.deltaTime,0);
+			yield return null;
+		}
+		Destroy (gameObject);
+	}
+}
