8e025a8200328de556a79e3445dfd2de961a236d
DHLife <dhlife09@gmail.com>
2016-10-20 18:50:36 +0900

    Add files via upload

---
+++ BlockGreen.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockGreen : MonoBehaviour {
+	public GameObject brokenBlocksPrefab;
+	public float hardness=5f;
+	void OnCollisionEnteR(Collision collisionInfo){
+		if (collisionInfo.relativeVelocity.magnitude >hardness) {
+			Instantiate(brokenBlocksPrefab, transform.position, brokenBlocksPrefab.transform.rotation);
+			Destroy (gameObject);
+		}
+	}
+}
